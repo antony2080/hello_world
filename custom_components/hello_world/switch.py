6193a71f12c6d7d35fc7a7398b9877e7b95d2d06
antony2080 <antony.zhang@luffanet.com.tw>
2025-06-24 11:23:44 +0800

    Add _attr_unique_id

--- custom_components/hello_world/switch.py
+++ custom_components/hello_world/switch.py
@@ -1,4 +1,7 @@
 from homeassistant.components.switch import SwitchEntity
+from homeassistant.helpers.device_registry import DeviceInfo
+
+DOMAIN = "hello_world"
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
@@ -9,6 +12,16 @@ class HelloWorldSwitch(SwitchEntity):
     def __init__(self):
         self._attr_name = "Hello World Switch"
         self._attr_is_on = False
+        self._attr_unique_id = "hello_world_switch"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        return DeviceInfo(
+            identifiers={(DOMAIN, "hello_world_device")},
+            name="Hello World",
+            manufacturer="Antony Inc.",
+            model="HW-1",
+        )
 
     async def async_turn_on(self, **kwargs):
         self._attr_is_on = True
