144d03cb4be434051fa48bd619c3506bc8baae68
antony2080 <antony.zhang@luffanet.com.tw>
2025-06-24 09:43:03 +0800

    First init

---
+++ custom_components/hello_world/__init__.py
@@ -0,0 +1,5 @@
+"""Hello World integration."""
+
+import logging
+
+_LOGGER = logging.getLogger(__name__)
---
+++ custom_components/hello_world/sensor.py
@@ -0,0 +1,18 @@
+from homeassistant.helpers.entity import Entity
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    async_add_entities([HelloWorldSensor()])
+
+
+class HelloWorldSensor(Entity):
+    def __init__(self):
+        self._state = "Hello World!"
+
+    @property
+    def name(self):
+        return "Hello World Sensor"
+
+    @property
+    def state(self):
+        return self._state
