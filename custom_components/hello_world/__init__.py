f720eaf850b1bd890506b861084d8c92f447c904
antony2080 <50128277+antony2080@users.noreply.github.com>
2025-08-05 15:54:14 +0800

    Update __init__.py

--- custom_components/hello_world/__init__.py
+++ custom_components/hello_world/__init__.py
@@ -1,5 +1,7 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import async_get_platforms
+
 from .camera import UrmetCamera
 from .button import ZoomButton
 from .const import DOMAIN
@@ -13,8 +15,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         "password": entry.data["password"],
     }
 
-    async_add_entities = hass.helpers.entity_platform.async_add_entities
-    async_add_entities(
-        [UrmetCamera(entry), ZoomButton(entry, "ZoomIn"), ZoomButton(entry, "ZoomOut")]
-    )
+    platforms = async_get_platforms(hass, DOMAIN)
+    for platform in platforms:
+        platform.async_add_entities(
+            [
+                UrmetCamera(entry),
+                ZoomButton(entry, "ZoomIn"),
+                ZoomButton(entry, "ZoomOut"),
+            ]
+        )
     return True
