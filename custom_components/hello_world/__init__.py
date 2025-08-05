a933cdab99763d558d7945a7c5a91f4d5a80babe
antony2080 <50128277+antony2080@users.noreply.github.com>
2025-08-05 15:59:16 +0800

    Update __init__.py

--- custom_components/hello_world/__init__.py
+++ custom_components/hello_world/__init__.py
@@ -1,13 +1,18 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import async_get_platforms
+import logging
 
 from .camera import UrmetCamera
 from .button import ZoomButton
 from .const import DOMAIN
 
+_LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    _LOGGER.info("Setting up entry: %s", entry.title)
+
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = {
         "host": entry.data["host"],
@@ -17,6 +22,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     platforms = async_get_platforms(hass, DOMAIN)
     for platform in platforms:
+        _LOGGER.info("Adding entities to platform: %s", platform.domain)
         platform.async_add_entities(
             [
                 UrmetCamera(entry),
