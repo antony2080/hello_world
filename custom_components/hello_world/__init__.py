db10b4d57668bf5a4f03cf144a84b2ffd33d673d
antony2080 <50128277+antony2080@users.noreply.github.com>
2025-08-05 15:20:44 +0800

    Add const py

--- custom_components/hello_world/__init__.py
+++ custom_components/hello_world/__init__.py
@@ -2,8 +2,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .camera import UrmetCamera
 from .button import ZoomButton
-
-DOMAIN = "hello_world"
+from .const import DOMAIN
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
--- custom_components/hello_world/config_flow.py
+++ custom_components/hello_world/config_flow.py
@@ -1,6 +1,5 @@
 from homeassistant import config_entries
 import voluptuous as vol
-
 from . import DOMAIN
 
 
