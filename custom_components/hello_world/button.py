db378952639f22041fb271ec6444195ad8458898
antony2080 <50128277+antony2080@users.noreply.github.com>
2025-08-07 15:37:53 +0800

    Update button.py

--- custom_components/hello_world/button.py
+++ custom_components/hello_world/button.py
@@ -14,18 +14,22 @@ class ZoomButton(ButtonEntity):
         self._attr_name = f"Camera {direction}"
         self._attr_unique_id = f"urmet_camera_{direction.lower()}"
         self._entry = entry
+        self._duration = 0.8
 
     async def async_press(self):
         data = self._hass.data[DOMAIN][self._entry.entry_id]
-        host = data["host"]
-        auth = aiohttp.BasicAuth(data["username"], data["password"])
+        ip = data["ip"]
+        username = data.get("username")
+        password = data.get("password")
+        auth = aiohttp.BasicAuth(username, password)
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
-        url = f"http://{host}/PTZ/1/{self._direction}"
-
+        url = f"http://{ip}/PTZ/1/{self._direction}"
+        payload_start = "Param1=1&Param2=5"
+        payload_stop = "Param1=0&Param2=5"
         async with aiohttp.ClientSession() as session:
-            await session.put(url, data="Param1=1&Param2=5", headers=headers, auth=auth)
-            await asyncio.sleep(0.4)
-            await session.put(url, data="Param1=0&Param2=5", headers=headers, auth=auth)
+            await session.put(url, data=payload_start, headers=headers, auth=auth)
+            await asyncio.sleep(self._duration)
+            await session.put(url, data=payload_stop, headers=headers, auth=auth)
 
 
 async def async_setup_entry(
