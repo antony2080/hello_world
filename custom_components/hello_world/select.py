ca873c865d456c7fe78f769f768afbc8f556fd8a
antony2080 <50128277+antony2080@users.noreply.github.com>
2025-08-21 16:18:18 +0800

    Update the style of option to capitalize

--- custom_components/hello_world/select.py
+++ custom_components/hello_world/select.py
@@ -6,7 +6,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .api import CameraLocalAPI
 from .const import DOMAIN
 
-IR_MODES = ["day", "night", "auto"]
+IR_MODES = {"day": "Day", "night": "Night", "auto": "Automatic"}
 
 
 class IrCutSelect(SelectEntity):
@@ -15,8 +15,8 @@ class IrCutSelect(SelectEntity):
         self._entry = entry
         self._attr_name = f"Camera {entry.data['name']} Mode"
         self._attr_unique_id = f"ircut_{entry.entry_id}"
-        self._attr_options = IR_MODES
-        self._current_option = "day"  # Default, or fetch from device
+        self._attr_options = list(IR_MODES.values())
+        self._current_option = IR_MODES["day"]  # Default, or fetch from device
 
     @property
     def current_option(self):
@@ -34,15 +34,19 @@ class IrCutSelect(SelectEntity):
 
     @property
     def icon(self):
-        if self._current_option == "day":
+        if self._current_option == IR_MODES["day"]:
             return "mdi:white-balance-sunny"
-        elif self._current_option == "night":
+        elif self._current_option == IR_MODES["night"]:
             return "mdi:weather-night"
-        elif self._current_option == "auto":
-            return "mdi:theme-light-dark"
+        elif self._current_option == IR_MODES["auto"]:
+            return "mdi:alpha-a-circle-outline"
         return "mdi:camera"
 
     async def async_select_option(self, option: str):
+        # Find the key for the selected display name
+        mode_key = next((k for k, v in IR_MODES.items() if v == option), None)
+        if not mode_key:
+            return
         data = self._hass.data[DOMAIN][self._entry.entry_id]
         ip = data["ip"]
         username = data.get("username")
@@ -50,7 +54,7 @@ class IrCutSelect(SelectEntity):
         auth = aiohttp.BasicAuth(username, password)
         headers = {"Content-Type": "application/xml"}
         url = f"http://{ip}/Images/1/IrCutFilter"
-        mode_value = "passivity" if option == "auto" else option
+        mode_value = "passivity" if mode_key == "auto" else mode_key
         xml = f"""<?xml version='1.0' encoding='UTF-8' ?>
 <IrCutFillter Version='1.0'>
     <Mode>{mode_value}</Mode>
@@ -63,7 +67,7 @@ class IrCutSelect(SelectEntity):
         async with aiohttp.ClientSession() as session:
             async with session.put(url, data=xml, headers=headers, auth=auth) as resp:
                 if resp.status == 200:
-                    self._current_option = option
+                    self._current_option = IR_MODES[mode_key]
         self.async_write_ha_state()
 
     async def async_update(self):
@@ -75,7 +79,9 @@ class IrCutSelect(SelectEntity):
         mode = await api.get_ircut_mode()
         if mode is not None:
             if mode == "passivity":
-                self._current_option = "auto"
+                self._current_option = IR_MODES["auto"]
+            elif mode in IR_MODES:
+                self._current_option = IR_MODES[mode]
             else:
                 self._current_option = mode
 
